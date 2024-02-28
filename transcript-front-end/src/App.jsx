86662d1ecdf5199e6464887aaf9e4f61d8819b15
Oliver Smith <madtownoliver@gmail.com>
2024-02-28 12:50:01 -0600

    Set up placeholder

---
+++ transcript-front-end/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+      <div>
+        <p>Placeholder</p>
+      </div>
+  )
+}
+
+export default App;
