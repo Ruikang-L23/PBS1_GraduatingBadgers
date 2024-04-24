e5ea087e49ec8a08de2d32675e42a61ac37cf81b
Shreyan Reddy Manyam <55506690+ShreyanReddy@users.noreply.github.com>
2024-04-24 11:39:37 -0500

    Updated homepage

--- transcript-front-end/src/components/Navigation.jsx
+++ transcript-front-end/src/components/Navigation.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import { Link, Outlet } from 'react-router-dom';
 import CurrentTranscriptContext from '../CurrentTranscriptContext';
+import logo from './images/logo.png'
 
 export default function Navigation() {
 
@@ -11,7 +12,15 @@ export default function Navigation() {
         <div>
             <Navbar bg="dark" variant="dark" sticky="top">
                 <Container>
-                    <Navbar.Brand as={Link} to="/">Transcript Translator</Navbar.Brand>
+                    <Navbar.Brand as={Link} to="/">
+                        <img
+                            src={logo}
+                            alt="PBS Wisconsin"
+                            width="25%" 
+                            height="auto" 
+                            className="d-inline-block align-top"
+                        />
+                    </Navbar.Brand>
                     <Nav>
                         <Nav.Link as={Link} to="/">Home</Nav.Link>
                         <Nav.Link as={Link} to="/upload">Upload</Nav.Link>
