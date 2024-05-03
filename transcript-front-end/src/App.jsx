ea59a92e2e5d850e4d51b29b429d3cacec87be44
Shreyan Reddy Manyam <55506690+ShreyanReddy@users.noreply.github.com>
2024-05-03 14:40:01 -0500

    added comments

--- transcript-front-end/src/App.jsx
+++ transcript-front-end/src/App.jsx
@@ -1,3 +1,4 @@
+// Import React components and Router elements
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navigation from "./components/Navigation";
@@ -6,16 +7,18 @@ import HomePage from "./components/HomePage";
 import Viewer from "./components/Viewer";
 import ErrorPage from "./components/ErrorPage";
 
+// Main App component defining the route structure
 export default function App() {
 
   return (
       <BrowserRouter>
+        {/* Setup the Navigation component as a layout with nested routes */}
         <Routes>
           <Route path="/" element={<Navigation />}>
-            <Route index element={<HomePage />}></Route>
-            <Route path="upload" element={<UploadPage />}></Route>
-            <Route path="viewer" element={<Viewer />}></Route>
-            <Route path="*" element={<ErrorPage />}></Route>
+            <Route index element={<HomePage />}></Route> // Home page at root
+            <Route path="upload" element={<UploadPage />}></Route> // Upload page
+            <Route path="viewer" element={<Viewer />}></Route> // Viewer page
+            <Route path="*" element={<ErrorPage />}></Route> // Fallback for unmatched routes
           </Route>
         </Routes>
       </BrowserRouter>
